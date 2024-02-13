a2192a54c3ae231fa3ba747c81aab049396a457d
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-02-13 01:22:34 +0000

    Mocks in Vue.js: intercepting functions

--- hello-world/tests/unit/hello_world.spec.js
+++ hello-world/tests/unit/hello_world.spec.js
@@ -25,4 +25,30 @@ describe('HelloWorld.vue', () => {
     expect(wrapper.find('#header-counter').text()).toBe('counter: 1')
     expect(wrapper.vm.counter).toBe(1)
   })
+
+  it('button click shlould call increment and counter data increment', async () => {
+    const spyIncrement = jest.spyOn(HelloWorldVue.methods, 'increment')
+
+    const wrapper = shallowMount(HelloWorldVue)
+
+    const component = wrapper.find('#but-increment')
+    await component.trigger('click')
+
+    expect(spyIncrement).toHaveBeenCalledTimes(1)
+    expect(wrapper.vm.counter).toBe(1)
+  })
+  it('button click should call increment function using mock', async () => {
+    const mockedIncrement = jest.fn()
+
+    const wrapper = shallowMount(HelloWorldVue)
+
+    wrapper.setMethods({ increment: mockedIncrement })
+
+    const component = wrapper.find('#but-increment')
+    await component.trigger('click')
+
+    expect(mockedIncrement).toHaveBeenCalledTimes(1)
+
+    expect(wrapper.vm.counter).toBe(0)
+  })
 })
